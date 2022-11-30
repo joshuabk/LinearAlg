0cca4672c4d9c438dc2c72613f829646d677c08c
Josh <joshuabkessler@yahoo.com>
2022-11-30 15:38:27 -0500

    laptop commit

--- LinearAlg.py
+++ LinearAlg.py
@@ -1,5 +1,6 @@
 import math
 import numpy as np
+import random as ran
 
 #class LinAlgebra:
 
@@ -8,17 +9,20 @@ import numpy as np
 
 
 def MatrixMult(mat1, mat2):
-        prodMat = np.zeros(int(mat1.shape[1]), int(mat2.shape[0]))
-        if mat1.shape[0] == mat2.shape[1] and  mat1.shape[1] == mat2.shape[2]:
+        prodMat = np.zeros((mat1.shape[1],mat2.shape[0]))
+        if mat1.shape[0] == mat2.shape[1] and  mat1.shape[1] == mat2.shape[0]:
 
             for x in range(mat1.shape[1]):
+                
                 for i in range(mat1.shape[0]):
                     sum = 0
                     for j in range(mat1.shape[1]):
-                        sum += mat1[j][x] * mat2.shape[i][j]
-                    prodMat[x][i] = sum
+                        
+                        sum +=  mat1[x][j] * mat2[j][i]
+                    #print(sum)
+                    prodMat[x][i] = int(sum)
                 
-            print(prodMat) 
+            print(prodMat)
             return prodMat
 
   
@@ -29,8 +33,33 @@ def MatrixMult(mat1, mat2):
 
 import numpy as np
 
-mata = np.array([[2,3], [3,5]])
-matb = np.array([[1,5], [3,4]])    
+mata = np.array([[2,3,4], [3,5,2], [2,3,4]])
+matb = np.array([[2,8,4], [3,5,9], [7,3,4]])    
 
 testM = MatrixMult(mata, matb)
+
+#print(testM)
+
+print(np.matmul(mata,matb))
+
+def makeMatrix(dim1, dim2):
+    
+    matrix = np.zeros((dim1, dim2))
+    for i in range(dim1):
+        for j in range(dim2):
+            matrix[i][j] = ran.randint(1,10)
+    return matrix
+
+
+mat1 = makeMatrix(7, 7)
+
+mat2 = makeMatrix(7, 7)
+
+print( MatrixMult(mat1, mat2))
+
+
+print( np.matmul(mat1, mat2))
+
+matrixAdd
+
 
